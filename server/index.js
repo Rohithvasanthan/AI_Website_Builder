8d70948b7a302e8fbfa7b00067be097880816341
Rohithvasanthan <rjrohith1803@gmail.com>
2026-07-08 23:28:07 +0530

    add database connection setup

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDb from "./config/db.js";
 dotenv.config()
 
 const app = express();
 const port = process.env.PORT || 5000
 
-app.listen(port , ()=>{
+
+
+
+app.listen(port ,async ()=>{
+
     console.log("Server Started");
+    await connectDb();
 })
