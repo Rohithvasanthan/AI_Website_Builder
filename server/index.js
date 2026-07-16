f62bf6c1209ef80459b9f69bbfc8e6039439c6f2
Rohithvasanthan <rjrohith1803@gmail.com>
2026-07-16 19:46:01 +0530

    feat: add authentication routes for Google login and logout

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
+import authRouter from "./routes/auth.routes.js";
 dotenv.config()
 
 const app = express();
 const port = process.env.PORT || 5000
 
+app.use(express.json())
+app.use("/api/auth",authRouter)
 
 
 
